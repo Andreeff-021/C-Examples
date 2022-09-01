d852c9b0371679f76cd78b298f86c1c488937be9
Владимир Андреев <andreeff.vladimir@yandex.ru>
2022-09-01 15:01:16 +0300

    example_010_method_array

---
+++ example_010_method_array/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 12, 24, 83, 14, 95, 26, 78, 81 };
+int n = array.Length;
+int find = 78;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
